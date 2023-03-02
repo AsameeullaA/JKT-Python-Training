58d430b2ec7452176285cb84ae35f825fb36ebd9
AsameeullaA <126562608+AsameeullaA@users.noreply.github.com>
2023-03-02 21:55:33 +0530

    Update Financial.py

--- day1/Financial.py
+++ day1/Financial.py
@@ -2,7 +2,10 @@ Year = int(input("Enter The Year: "))
 Month = int(input("Enter The Month: "))
 if(Month > 4) :
     financial_Year = str(Year) + "-" + str(Year+1)
+    assesment_year = str(Year+1) + "-" + str(Year+2)
 else:
     financial_Year = str(Year-1) + "-" + str(Year)
+    assesment_year = str(Year) + "-" + str(Year+1)
 
 print("The Financial year is", financial_Year)
+print("The Assesment yeat is ", assesment_year)
